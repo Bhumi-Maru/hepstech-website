68942be63c65b99cf84a37ef0b79eb1f359fabbd
Bhumi-Maru <bhumimaru7@gmail.com>
2025-02-06 16:20:13 +0530

    commit0602

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
+
+const db = mongoose.connection;
+
+module.exports = db;
