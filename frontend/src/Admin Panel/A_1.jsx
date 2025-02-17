bb58a9906bc815c8948d5a0d7276b62314836a06
Bhumi-Maru <bhumimaru7@gmail.com>
2025-02-17 11:02:37 +0530

    commit17_02

--- frontend/src/Admin Panel/A_1.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function A_1() {
-  return <div>A_1</div>;
-}
--- frontend/src/Admin_Panel_App.js
+++ frontend/src/Admin_Panel_App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import A_1 from "./Admin Panel/A_1";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./Admin Panel/Sidebar/Sidebar";
 
 export default function Admin_Panel_App() {
   return (
-    <div>
-      <h1>Admin_Panel_App</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/hello" element={<A_1 />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="min-h-screen overflow-x-hidden font-sans antialiased text-gray-900 bg-gray-50 selection:bg-skin-primary selection:text-white">
+      {/* <!-- START WRAPPER --> */}
+      <div class="flex h-screen overflow-hidden">
+        <BrowserRouter>
+          <Sidebar />
+          <main className="flex flex-col flex-1 w-0 bg-gray-50"></main>
+        </BrowserRouter>
+      </div>
+      {/* <!-- END WRAPPER --> */}
     </div>
   );
 }
