a8fd8e5d42908a9e24e97c036b26029e75833f9e
BhumiMaru <bhumi.tryangletech@gmail.com>
2025-03-04 13:02:14 +0530

    commit04_03

--- frontend/src/Admin Panel/context/All_Media_Context.jsx
+++ frontend/src/Admin Panel/context/All_Media_Context.jsx
@@ -9,7 +9,6 @@ export const useAllMediaContext = () => {
 
 export const AllMediaProvider = ({ children }) => {
   const [mediaItems, setMediaItems] = useState([]);
-  const [image, setImage] = useState(null);
   const [selectedDateRange, setSelectedDateRange] = useState("");
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [searchTerm, setSearchTerm] = useState(""); // Search term state
@@ -22,39 +21,6 @@ export const AllMediaProvider = ({ children }) => {
       item.imageUrl.toLowerCase().includes(searchTerm.toLowerCase()) // You can extend this to search by other attributes
   );
 
-  const handleImageChange = (e) => {
-    if (e.target.files.length > 0) {
-      setImage(e.target.files[0]);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (!image) {
-      alert("Please select an image first!");
-      return;
-    }
-    const formData = new FormData();
-    formData.append("file", image);
-    try {
-      const response = await axios.post(
-        "http://localhost:7000/api/upload",
-        formData,
-        { headers: { "Content-Type": "multipart/form-data" } }
-      );
-      const newImage = {
-        name: image.name,
-        size: (image.size / 1024 / 1024).toFixed(2) + " MB",
-        imageUrl: `http://localhost:7000${response.data.file.filePath}`,
-      };
-      setMediaItems((prevItems) => [...prevItems, newImage]);
-      alert("Upload successful: " + response.data.message);
-      setImage(null);
-    } catch (error) {
-      console.error("Upload failed:", error);
-      alert("Upload failed. Please try again.");
-    }
-  };
-
   const handleDateSelect = (range) => {
     setSelectedDateRange(range);
     setShowDatePicker(false);
@@ -65,9 +31,6 @@ export const AllMediaProvider = ({ children }) => {
       value={{
         mediaItems: filteredMediaItems, // Provide filtered media items
         setMediaItems,
-        image,
-        handleImageChange,
-        handleUpload,
         selectedDateRange,
         setSelectedDateRange,
         showDatePicker,
